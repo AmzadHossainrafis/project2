2a5caabaeba6e7d6f70277fd2bcebb8d47bae020
Amzad hossain <amzad.rafi@northsouth.edu>
2022-08-18 14:22:51 +0600

    dice loss function

---
+++ metrices/dice_loss.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.0
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2.0 * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth) 
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = tf.reshape(y_true, [-1])
+    y_pred_f = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2.0 * intersection) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f))
+
+
+
+
